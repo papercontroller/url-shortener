aa0825ebd44113ee79fc080017f5505156fa1b21
Sanya420 <sanjarmaulkan2102@mail.ru>
2025-01-22 10:45:52 +0500

    logger added

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,37 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
 	"main/internal/config"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
 )
 
 func main() {
 	cfg := config.MustLoad()
 	fmt.Println(cfg)
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting url-shortener", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	}
+	return log
 }
